aed4848c73b52d8613e93f17e51cec324a088d25
prav <prablemo@outlook.com>
2022-04-13 07:53:12 +0545

    removing unwanted comments

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-//display hello tej when website is loaded
+
 document.write("Hello Tej");
-//function to increment or decrement the value of html element when clicked on the button
+
 function increment() {
     var value = document.getElementById("value").innerHTML;
     value = parseInt(value) + 1;
